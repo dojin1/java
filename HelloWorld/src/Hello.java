a11365093902c31d75d3931ec4c4ba5853e39f40
ana27 <ana27@daum.net>
2021-07-22 17:36:01 +0900

    0722 commit

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,10 +5,22 @@ public class Hello {
 	public static void main(String[] args) {
 	Scanner s=new Scanner(System.in);
 		String str=s.nextLine();
-		if(str.equals("cjh"))
-			//문자열 같은가? equals
-			//IgnoreCase 소문자 대문자 
-		System.out.println("cho jun ho");
+		/*if(str.equals("a") || str.equals("A")) {
+			System.out.println("Apple");
+			} else if(str.equals("b") || str.equals("B")) {
+			System.out.println("Banana");
+			} else if(str.equals("w") || str.equals("W")) {
+			System.out.println("Watermelon");
+			}  else if(str.equals("s") || str.equals("S")) {
+				System.out.println("Strawberry");
+			}else {
+				System.out.println("모르는 이니셜");
+			}
+			System.out.println("프로그램 종료");
+		*/
+	
+		
+			
 	}
 
 }

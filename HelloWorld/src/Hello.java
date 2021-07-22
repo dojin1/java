f9e37c2945e0bf95d4c72567e78edc806888620c
ana27 <ana27@daum.net>
2021-07-22 11:16:41 +0900

    0722 11:16

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Jane\n28\n");
+		System.out.print("dojin\n28\n");
 		System.out.print("World");
 	}
 

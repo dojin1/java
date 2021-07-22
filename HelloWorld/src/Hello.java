8bf89646e0a6d9970466fd26a5178f2f38930381
chojunho <ana27@daum.net>
2021-07-22 11:13:52 +0900

    0722 3class commit

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("John\n28\n");
+		System.out.print("Jane\n28\n");
 		System.out.print("World");
 	}
 

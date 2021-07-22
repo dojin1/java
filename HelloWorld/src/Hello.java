e327b04623f75721781e9ea885d89c64741852ea
ana27 <ana27@daum.net>
2021-07-22 13:51:50 +0900

    0722 4class

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("dojin\n28\n");
-		System.out.print("World");
+		System.out.print(1>=1);
 	}
 
 }

6e710d7f813a0005ac79fabe5438b49d88ac1af5
ana27 <ana27@daum.net>
2021-07-26 11:04:40 +0900

    0726 3

--- HelloWorld/src/Hello4.java
+++ HelloWorld/src/Hello4.java
@@ -2,8 +2,14 @@
 public class Hello4 {
 
 	public static void main(String[] args) {
-		// for문
-
+		// for문 for(1:초기화,2:조건,4:증감)
+		//				3:실행문
+		// [100이하의 홀수 출력]
+		for (int n= 1; n <= 100; n++) {
+			if (n%2 == 1) {
+				System.out.println(n);
+			}
+		}
 	}
 
 }

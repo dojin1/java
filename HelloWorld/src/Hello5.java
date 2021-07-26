07143c3eb70190e7022f8f9133d0d33b6c6049da
ana27 <ana27@daum.net>
2021-07-26 11:50:29 +0900

    0726 3class

--- HelloWorld/src/Hello5.java
+++ HelloWorld/src/Hello5.java
@@ -2,8 +2,15 @@
 public class Hello5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// for문안에 for문을 넣어서
+		// 구구단을 2단부터 9단까지 출력
+		for (int r = 2; r <= 9; r++) {
+			System.out.println("-"+ r + "단-");
+		
+			for (int n = 1; n <= 9; n++) {
+				System.out.println(r + " x " + n + " = " + r*n);
+			}
+		}
 	}
 
 }

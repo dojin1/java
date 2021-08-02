b1a5b283d3e8872857ccb11dbae2882be5e77ecf
ana27 <ana27@daum.net>
2021-08-02 15:44:38 +0900

    0802

--- HelloWorld/src/Sddd.java
+++ HelloWorld/src/Sddd.java
@@ -3,23 +3,12 @@ import java.util.Scanner;
 public class Sddd {
 
 	public static void main(String[] args) {
-		// for문으로 1부터 10까지 출력
-	Scanner s=new Scanner(System.in);
-	int a, b, max=0, min=0, i=2;
-	System.out.println("두 수 입력 : ");
-	a = s.nextInt();
-	b = s.nextInt();
-	for(; i<=a && i<=b; i++) {
-		if(a%i==0 && b%i==0) 
-			min=1;
+		for(int i=2; i<10; i++) {
+			for(int a=1; a<10; a++) {
+				System.out.println(i+"X"+a+"="+i*a);
+			}
 			
-		
-			
-		
-	}
-	max=a*b/min;
-	System.out.println("최소공배수 : " + max);
-	System.out.println("최대공약수 : " + min);
+		}
 	}
 
 }
